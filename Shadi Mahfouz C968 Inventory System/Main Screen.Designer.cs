b0cb211bdd648b9c977a56a570d3e3298a9a4110
shadimahfouz <58312547+shadimahfouz@users.noreply.github.com>
2021-02-17 15:55:36 -0500

    Add files via upload

---
+++ Shadi Mahfouz C968 Inventory System/Main Screen.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Shadi_Mahfouz_C968_Inventory_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SearchParts = new System.Windows.Forms.Button();
+            this.SearchProducts = new System.Windows.Forms.Button();
+            this.AddParts = new System.Windows.Forms.Button();
+            this.ModifyParts = new System.Windows.Forms.Button();
+            this.DeleteParts = new System.Windows.Forms.Button();
+            this.AddProducts = new System.Windows.Forms.Button();
+            this.ModifyProducts = new System.Windows.Forms.Button();
+            this.DeleteProducts = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(455, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(473, 24);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Inventory Management System Home Page";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(559, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(274, 13);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Shadi Mahfouz | ID# 001423923 | 02/17/2021";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(650, 370);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(717, 154);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(650, 370);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(717, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 19);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Products";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox4.Location = new System.Drawing.Point(34, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 19);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Parts";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(477, 128);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(207, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1160, 128);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(207, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // SearchParts
+            // 
+            this.SearchParts.Location = new System.Drawing.Point(396, 127);
+            this.SearchParts.Name = "SearchParts";
+            this.SearchParts.Size = new System.Drawing.Size(75, 20);
+            this.SearchParts.TabIndex = 8;
+            this.SearchParts.Text = "Search";
+            this.SearchParts.UseVisualStyleBackColor = true;
+            // 
+            // SearchProducts
+            // 
+            this.SearchProducts.Location = new System.Drawing.Point(1079, 127);
+            this.SearchProducts.Name = "SearchProducts";
+            this.SearchProducts.Size = new System.Drawing.Size(75, 20);
+            this.SearchProducts.TabIndex = 9;
+            this.SearchProducts.Text = "Search";
+            this.SearchProducts.UseVisualStyleBackColor = true;
+            // 
+            // AddParts
+            // 
+            this.AddParts.Location = new System.Drawing.Point(425, 530);
+            this.AddParts.Name = "AddParts";
+            this.AddParts.Size = new System.Drawing.Size(75, 30);
+            this.AddParts.TabIndex = 10;
+            this.AddParts.Text = "Add";
+            this.AddParts.UseVisualStyleBackColor = true;
+            // 
+            // ModifyParts
+            // 
+            this.ModifyParts.Location = new System.Drawing.Point(506, 530);
+            this.ModifyParts.Name = "ModifyParts";
+            this.ModifyParts.Size = new System.Drawing.Size(75, 30);
+            this.ModifyParts.TabIndex = 11;
+            this.ModifyParts.Text = "Modify";
+            this.ModifyParts.UseVisualStyleBackColor = true;
+            // 
+            // DeleteParts
+            // 
+            this.DeleteParts.Location = new System.Drawing.Point(587, 530);
+            this.DeleteParts.Name = "DeleteParts";
+            this.DeleteParts.Size = new System.Drawing.Size(75, 30);
+            this.DeleteParts.TabIndex = 12;
+            this.DeleteParts.Text = "Delete";
+            this.DeleteParts.UseVisualStyleBackColor = true;
+            // 
+            // AddProducts
+            // 
+            this.AddProducts.Location = new System.Drawing.Point(1127, 533);
+            this.AddProducts.Name = "AddProducts";
+            this.AddProducts.Size = new System.Drawing.Size(75, 30);
+            this.AddProducts.TabIndex = 13;
+            this.AddProducts.Text = "Add";
+            this.AddProducts.UseVisualStyleBackColor = true;
+            // 
+            // ModifyProducts
+            // 
+            this.ModifyProducts.Location = new System.Drawing.Point(1208, 533);
+            this.ModifyProducts.Name = "ModifyProducts";
+            this.ModifyProducts.Size = new System.Drawing.Size(75, 30);
+            this.ModifyProducts.TabIndex = 14;
+            this.ModifyProducts.Text = "Modify";
+            this.ModifyProducts.UseVisualStyleBackColor = true;
+            // 
+            // DeleteProducts
+            // 
+            this.DeleteProducts.Location = new System.Drawing.Point(1289, 533);
+            this.DeleteProducts.Name = "DeleteProducts";
+            this.DeleteProducts.Size = new System.Drawing.Size(75, 30);
+            this.DeleteProducts.TabIndex = 15;
+            this.DeleteProducts.Text = "Delete";
+            this.DeleteProducts.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(1289, 594);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 30);
+            this.Exit.TabIndex = 16;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 646);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.DeleteProducts);
+            this.Controls.Add(this.ModifyProducts);
+            this.Controls.Add(this.AddProducts);
+            this.Controls.Add(this.DeleteParts);
+            this.Controls.Add(this.ModifyParts);
+            this.Controls.Add(this.AddParts);
+            this.Controls.Add(this.SearchProducts);
+            this.Controls.Add(this.SearchParts);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Main Screen";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button SearchParts;
+        private System.Windows.Forms.Button SearchProducts;
+        private System.Windows.Forms.Button AddParts;
+        private System.Windows.Forms.Button ModifyParts;
+        private System.Windows.Forms.Button DeleteParts;
+        private System.Windows.Forms.Button AddProducts;
+        private System.Windows.Forms.Button ModifyProducts;
+        private System.Windows.Forms.Button DeleteProducts;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
